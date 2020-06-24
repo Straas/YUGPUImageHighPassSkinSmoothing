4eb8aae32add5768ca4c583fa69f8c78350ebb91
allenlinli <allenlinli@gmail.com>
2020-06-24 20:04:25 +0800

    feat: update podspec and Podfile. Did pod install

---
+++ YUGPUImageHighPassSkinSmoothingDemo/Pods/Target Support Files/GPUImage-StraaS/GPUImage-StraaS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GPUImage_StraaS : NSObject
+@end
+@implementation PodsDummy_GPUImage_StraaS
+@end
---
+++ YUGPUImageHighPassSkinSmoothingDemo/Pods/Target Support Files/YUGPUImageHighPassSkinSmoothing-Straas/YUGPUImageHighPassSkinSmoothing-Straas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YUGPUImageHighPassSkinSmoothing_Straas : NSObject
+@end
+@implementation PodsDummy_YUGPUImageHighPassSkinSmoothing_Straas
+@end
